db82d30c482f2c9311b2432494c0e6faf7981870
pooyaaf <pooya1380@gmail.com>
2023-10-18 15:56:17 +0330

    add user tests

--- CA1/src/test/java/user/UserTest.java
+++ CA1/src/test/java/user/UserTest.java
@@ -3,6 +3,7 @@ package user;
 import exceptions.CommodityIsNotInBuyList;
 import exceptions.InsufficientCredit;
 import exceptions.InvalidCreditRange;
+import model.Commodity;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -17,13 +18,13 @@ public class UserTest {
     }
 
     @Test
-    void test_add_normal_credit() throws InvalidCreditRange {
+    void test_add_credit() throws InvalidCreditRange {
         user.addCredit(100);
         Assertions.assertEquals(100, user.getCredit(), 100);
     }
 
     @Test
-    void reject_add_negative_credit() {
+    void reject_negative_credit() {
         Assertions.assertThrows(InvalidCreditRange.class, () -> {
             user.addCredit(-50);
         });
@@ -34,6 +35,55 @@ public class UserTest {
         user.addCredit(200);
         user.withdrawCredit(100);
         Assertions.assertEquals(100, user.getCredit());
+    }
+
+    @Test
+    void reject_withdraw_more_credit_than_available() throws InvalidCreditRange {
+        user.addCredit(100);
+        Assertions.assertThrows(InsufficientCredit.class, () -> {
+            user.withdrawCredit(200);
+        });
+    }
+
+    @Test
+    void test_add_item_to_buyList(){
+        Commodity commodity = new Commodity();
+        commodity.setId("1");
+        user.addBuyItem(commodity);
+        Assertions.assertEquals(1, user.getBuyList().get("1"));
+
+    }
+
+    @Test
+    void test_add_item_to_buyList_without_id(){
+        Commodity commodity = new Commodity();
+        user.addBuyItem(commodity);
+        Assertions.assertEquals(null, user.getBuyList().get("null"));
+    }
+
+
+    @Test
+    void test_add_purchasedItem() {
+        user.addPurchasedItem("commodityId", 20);
+        Assertions.assertEquals(20, user.getPurchasedList().get("commodityId"));
+    }
+
+    @Test
+    void test_remove_item_from_buyList() throws CommodityIsNotInBuyList {
+        Commodity commodity = new Commodity();
+        commodity.setId("commodityId");
+        user.addBuyItem(commodity);
+        user.removeItemFromBuyList(commodity);
+        Assertions.assertNull(user.getBuyList().get("commodityId"));
+    }
+
+    @Test
+    void reject_removing_non_existing_item_from_buyList(){
+        Commodity commodity = new Commodity();
+        commodity.setId("commodityId");
+        Assertions.assertThrows(CommodityIsNotInBuyList.class, () -> {
+            user.removeItemFromBuyList(commodity);
+        });
     }
 
     @AfterEach
