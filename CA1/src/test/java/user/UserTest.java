e5a61969124ced0843cbe4e93bb3aebc10236d59
pooyaaf <pooya1380@gmail.com>
2023-10-18 12:42:56 +0330

    add more tests to user

--- CA1/src/test/java/user/UserTest.java
+++ CA1/src/test/java/user/UserTest.java
@@ -15,10 +15,29 @@ public class UserTest {
     void setUp() {
         user = new User("username", "password", "email", "01/01/1899", "1 St Jones");
     }
+
     @Test
-    void testAddCredit() throws InvalidCreditRange {
+    void test_add_normal_credit() throws InvalidCreditRange {
         user.addCredit(100);
         Assertions.assertEquals(100, user.getCredit(), 100);
     }
-    // AfterEach
+
+    @Test
+    void reject_add_negative_credit() {
+        Assertions.assertThrows(InvalidCreditRange.class, () -> {
+            user.addCredit(-50);
+        });
+    }
+
+    @Test
+    void test_withdraw_credit() throws InvalidCreditRange, InsufficientCredit {
+        user.addCredit(200);
+        user.withdrawCredit(100);
+        Assertions.assertEquals(100, user.getCredit());
+    }
+
+    @AfterEach
+    public void teardown() {
+        user = null;
+    }
 }
