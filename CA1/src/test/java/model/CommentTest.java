6fe6100638db1100fcb2fb8afa4dafc2956d48da
Mostafa Ebrahimi <mostafa.ebrahimi2002@gmail.com>
2023-10-24 12:47:09 +0330

    src/test/java/model/CommentTest.java: Added 'testConstructor'

--- CA1/src/test/java/model/CommentTest.java
+++ CA1/src/test/java/model/CommentTest.java
@@ -1,7 +1,11 @@
 package model;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class CommentTest {
     private Comment comment;
@@ -16,10 +20,23 @@ public class CommentTest {
         comment.setText("text");
     }
 
+    @ParameterizedTest
+    @CsvSource({
+            "2, userEmail2, username2, 2, text2",
+            "3, userEmail3, username3, 3, text3",
+            "4, userEmail4, username4, 4, text4",
+    })
+    void testConstructor(int id, String userEmail, String username, int commodityId, String text) {
+        Comment comment = new Comment(id, userEmail, username, commodityId, text);
+        Assertions.assertEquals(id, comment.getId());
+        Assertions.assertEquals(userEmail, comment.getUserEmail());
+        Assertions.assertEquals(username, comment.getUsername());
+        Assertions.assertEquals(commodityId, comment.getCommodityId());
+        Assertions.assertEquals(text, comment.getText());
+    }
 
     @AfterEach
     void tearDown() {
         comment = null;
     }
-
 }
