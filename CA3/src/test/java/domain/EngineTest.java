05864109f2227f3b13d4c29d9760f3853f3f7134
Mostafa Ebrahimi <mostafa.ebrahimi2002@gmail.com>
2023-11-11 15:54:41 +0330

    EngingTest.java: Added testGetAverageOrderQuantityByCustomer & setup & tearDwong

--- CA3/src/test/java/domain/EngineTest.java
+++ CA3/src/test/java/domain/EngineTest.java
@@ -1,11 +1,55 @@
 package domain;
 
-import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class EngineTest {
 
-    @Test
-    public void justAnExample() {
-        // TODO
+    private Engine engine;
+
+    @BeforeEach
+    void setUp() {
+        engine = new Engine();
     }
+    @AfterEach
+    void tearDown() {
+        engine = null;
+    }
+
+    @Test
+    public void testEngine() {
+        Assertions.assertTrue(engine.orderHistory.isEmpty());
+    }
+
+    @Test
+    public void testGetAverageOrderQuantityByCustomer_NoOrders() {
+        Assertions.assertEquals(0, engine.getAverageOrderQuantityByCustomer(1));
+    }
+
+    @Test
+    public void testGetAverageOrderQuantityByCustomer_SomeOrders() {
+        int ID_customer = 1;
+        int ID_customer2 = 2;
+
+        Order order1 = new Order();
+        order1.setCustomer(ID_customer);
+        order1.setQuantity(2);
+        Order order2 = new Order();
+        order2.setCustomer(ID_customer2);
+        order2.setQuantity(4);
+
+        engine.orderHistory.add(order1);
+        engine.orderHistory.add(order2);
+
+        Assertions.assertEquals(2, engine.getAverageOrderQuantityByCustomer(ID_customer));
+    }
+
+    @Test
+    public void testGetQuantityPatternByPrice_NoOrders() {
+        Assertions.assertEquals(0, engine.getQuantityPatternByPrice(1));
+    }
+
+    // TODO: testGetQuantityPatternByPrice_SomeOrders
 }
