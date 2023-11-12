de11797edb9911160ee07416f25241f78ff35d9d
Mostafa Ebrahimi <mostafa.ebrahimi2002@gmail.com>
2023-11-12 08:06:41 +0330

    EngineTest.java: Added testgetQuantityPatternByPrice()

--- CA3/src/test/java/domain/EngineTest.java
+++ CA3/src/test/java/domain/EngineTest.java
@@ -67,5 +67,98 @@ public class EngineTest {
         Assertions.assertEquals(0, engine.getQuantityPatternByPrice(1));
     }
 
-    // TODO: testGetQuantityPatternByPrice_SomeOrders
+    @Test
+    public void testGetQuantityPatternByPrice_WithoutSamePrice() {
+        int price = 15;
+
+        Order order1 = new Order();
+        order1.setId(1);
+        order1.setPrice(price);
+        order1.setQuantity(2);
+
+        Order order2 = new Order();
+        order2.setId(2);
+        order2.setPrice(10);
+        order2.setQuantity(4);
+
+        engine.orderHistory.add(order1);
+        engine.orderHistory.add(order2);
+
+        Assertions.assertEquals(0, engine.getQuantityPatternByPrice(price));
+    }
+
+    @Test
+    public void testGetQuantityPatternByPrice_Wit1SamePrice() {
+        int price = 15;
+
+        Order order1 = new Order();
+        order1.setId(1);
+        order1.setPrice(price);
+        order1.setQuantity(2);
+
+        Order order2 = new Order();
+        order2.setId(2);
+        order2.setPrice(price);
+        order2.setQuantity(4);
+
+        engine.orderHistory.add(order1);
+        engine.orderHistory.add(order2);
+
+        int expected = order2.getQuantity() - order1.getQuantity();
+        Assertions.assertEquals(expected, engine.getQuantityPatternByPrice(price));
+    }
+
+    @Test
+    public void testGetQuantityPatternByPrice_WithMultipleSamePrice_SameQuantity() {
+        int price = 15;
+
+        Order order1 = new Order();
+        order1.setId(1);
+        order1.setPrice(price);
+        order1.setQuantity(2);
+
+        Order order2 = new Order();
+        order2.setId(2);
+        order2.setPrice(price);
+        order2.setQuantity(4);
+
+        Order order3 = new Order();
+        order3.setId(3);
+        order3.setPrice(price);
+        order3.setQuantity(6);
+
+        engine.orderHistory.add(order1);
+        engine.orderHistory.add(order2);
+        engine.orderHistory.add(order3);
+
+        int expected = order2.getQuantity() - order1.getQuantity();
+        Assertions.assertEquals(expected, engine.getQuantityPatternByPrice(price));
+    }
+
+    @Test
+    public void testGetQuantityPatternByPrice_WithMultipleSamePrice_DifferentQuantity() {
+        int price = 15;
+
+        Order order1 = new Order();
+        order1.setId(1);
+        order1.setPrice(price);
+        order1.setQuantity(2);
+
+        Order order2 = new Order();
+        order2.setId(2);
+        order2.setPrice(price);
+        order2.setQuantity(4);
+
+        Order order3 = new Order();
+        order3.setId(3);
+        order3.setPrice(price);
+        order3.setQuantity(5);
+
+        engine.orderHistory.add(order1);
+        engine.orderHistory.add(order2);
+        engine.orderHistory.add(order3);
+
+        Assertions.assertEquals(0, engine.getQuantityPatternByPrice(price));
+    }
+
 }
