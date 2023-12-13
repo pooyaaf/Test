6c7bd8ded9fef93610e546edab885bf4ca1ee4ba
pooyaaf <pooya1380@gmail.com>
2023-12-13 10:57:00 +0330

    set pom and OrderTest

--- src/test/java/domain/OrderTest.java
+++ src/test/java/domain/OrderTest.java
@@ -8,5 +8,53 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class OrderTest {
 
+    private Order order;
 
+    @BeforeEach
+    void setup() {
+        order = new Order();
+        order.setId(1);
+        order.setCustomer(1);
+        order.setPrice(100);
+        order.setQuantity(10);
+    }
+
+    @AfterEach
+    void tearDown() {
+        order = null;
+    }
+
+    @Test
+    void testEquals_IsOrderSameID() {
+        Order order2 = new Order();
+        order2.setId(1);
+        assertTrue(order.equals(order2));
+    }
+
+    @Test
+    void testEquals_IsOrderDifferentID() {
+        Order order2 = new Order();
+        order2.setId(2);
+        assertFalse(order.equals(order2));
+    }
+
+    @Test
+    void testEquals_IsNotOrder() {
+        assertFalse(order.equals(new Object()));
+    }
+
+    @Test
+    void testGetId() {
+        assertEquals(1, order.getId());
+    }
+
+    @Test
+    void testGetCustomer() {
+        assertEquals(1, order.getCustomer());
+    }
+
+    @Test
+    void testGetPrice() {
+        assertEquals(100, order.getPrice());
+    }
 }
